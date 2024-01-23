09ccad4a8498b06b4dd43f8f6f0c2c70c8215725
Torham <87639984+TorhamDev@users.noreply.github.com>
2024-01-23 17:07:48 +0330

    Revert "update&_&"

--- idk/main.py
+++ idk/main.py
@@ -7,19 +7,15 @@ import random
 
 clock = pygame.time.Clock()
 running = True
-screen = pygame.display.set_mode((400, 520))
+screen = pygame.display.set_mode((400, 400))
 rect1 = pygame.Rect(80, 80, 40, 40)
 rect2 = pygame.Rect(130, 20, 40, 40)
 rect3 = pygame.Rect(180, 140, 40, 40)
 rect4 = pygame.Rect(220, 190, 40, 40)
-crash_sound = pygame.mixer.Sound("idk/s.mp3")
+crash_sound = pygame.mixer.Sound("./s.mp3")
 
 dt = 0
 move_letf = False
-
-border_closing_event = pygame.USEREVENT + 1
-timer_interval = 350  # Adjust this value to control the speed of the borders closing
-pygame.time.set_timer(border_closing_event, timer_interval)
 
 class MoveRect:
     def __init__(self, rect) -> None:
@@ -42,7 +38,6 @@ while running:
     for rect_name, rect in rects.items():
         print(rect_name, rect.rect.x, rect.rect.y, rect.move_left)
         rect.rect.clamp_ip(screen.get_rect())
-        window_size = pygame.display.get_window_size()
 
         if rect.rect.x <= 0:
             
@@ -51,7 +46,7 @@ while running:
             rect.move_left = False
             rect.speed = random.randint(1, 10)
             
-        if rect.rect.x >= window_size[0]-40:
+        if rect.rect.x >= 360:
             pygame.mixer.Sound.play(crash_sound)
             pygame.mixer.music.stop()
             rect.move_left = True
@@ -66,10 +61,7 @@ while running:
         if rect.move_left is False:
             rect.rect.x += rect.speed
 
-        
-        if event.type == border_closing_event:
-            if window_size[0] > 150:
-                pygame.display.set_mode((window_size[0]-0.1,window_size[1]-0.1))
+
 
 
     screen.fill("black")
